d5c4611a4c0a4d9c05563c78f84fff3e89c6c181
Cody Laminack <cody.laminack@gmail.com>
2016-05-13 15:14:03 -0400

    Model up to 68%. Lots of dummies added

--- kobe.py
+++ kobe.py
@@ -16,7 +16,8 @@ kobe.head(30)
 kobe2.shot_type.value_counts() # This only includes 2s and 3s, which might not be the most useful thing, especially if we can get it by creating dummies for
 #another variable.
 
-# What do the minutes_remaining and secons_remaining describe. Is seconds shot clock?
+# What do the minutes_remaining and secons_remaining describe. Is seconds shot clock? It's not
+# They're both the time left in the quarter.
 
 #This describes the minutes remaining in the quarter
 kobe2.minutes_remaining.value_counts()
@@ -25,12 +26,23 @@ kobe2.seconds_remaining.value_counts()
 # This is seconds remaining in the quarter, not on the shot clock. It probably won't be the most useful thing in the world.
 # Get dummy variables for season
 season_dummies = pd.get_dummies(kobe2.season)
-kobe2['home_or_away'] = 
-
 #join the dummies dataframe onto the original
 kobe2 = kobe2.join(season_dummies)
 
 
+# Make a column that shows whether or not a game was at home
+kobe2.iloc[0].matchup
+match_on = 'LAL @'
+kobe2['home'] = np.where(kobe2['matchup'].str.startswith('LAL @'), 0, 1) # matches the string and fills with a zero where True, and a one where False
+
+# Create a column that combines minutes and seconds remaining.
+kobe2['time_left'] = kobe2['minutes_remaining']+(kobe2.seconds_remaining/60.0)
+
+# Convert the shot_type column to have integer values for the type of shot.
+kobe2.shot_type = kobe2.shot_type.map({'2PT Field Goal':2, '3PT Field Goal':3})
+
+
+# Based on the below, it looks like game id and game date could both be used for indexing individual games.
 kobe2.game_id.value_counts().count()
 kobe2.game_date.nunique()
 
@@ -51,15 +63,58 @@ plt.show()
 kobe_plot = sns.pairplot(kobe2, hue='shot_made_flag', vars = ['loc_x', 'loc_y', 'period', 'seconds_remaining', 'shot_distance', 'shot_made_flag', 'playoffs'])
 kobe_plot.savefig('kobe_pair_plot.png')
 
+# After looking for some more useful variables. I think it might be useful to create dummies
+# for the 'combined shot type' and see if that helps.
+action_dummies = pd.get_dummies(kobe2.action_type)
+action_cols = action_dummies.columns
+kobe2 = kobe2.join(action_dummies)
 
-# Testing out some groupby functions
+# Create a column for days off
+# The first thing we're going to have to do is translate the game date from an object to a date
 
-games = kobe2.groupby('game_date')
+kobe2.game_date = kobe2.game_date.astype('datetime64')
+# Now, we should be able to create this new calculated column using a groupby
+
+games = kobe2.groupby('game_id', as_index=False)
 
 # Does the mean here give you average field goal percentage for that game?
+
+# In order to get this number, we're going to have to calculate it in a grouped thing,
+# convert those results to a dictionary, and then map that dictionary over the original df.
 games = games.agg({'shot_made_flag': [np.sum, np.mean],
-                    'opponent': lambda x: x.iloc[0]})
-kobe2.game_event_id.nunique()
+                    'opponent': lambda x: x.iloc[0],
+                    'game_date':lambda x:x.iloc[0]})
+games.columns = ['_'.join(col).strip() for col in games.columns.values]
+games.columns
+games['days_off'] = games['game_date_<lambda>'] - games['game_date_<lambda>'].shift() #With this calculation, 1 day off
+# means that it was a b2b.
+games.days_off.sort_values()
+# We need some # of days that will be a cutoff. Maybe 7 days?
+days_off_dict = {}
+days_off_dict['games.game_date_<lambda>'] = games.days_off
+
+days_off_dict
+a=[]
+for row in games.game_id_:
+    a.append(row)
+b=[]
+for row in games.days_off:
+    b.append(row)
+
+print b
+
+days_off_dict = dict(zip(a,b))
+days_off_dict
+
+kobe2.game_id.head()
+kobe2['days_off'] = kobe2.game_id
+kobe2['days_off'] = kobe2.game_id.map(days_off_dict)
+kobe2.days_off.fillna(100, inplace=True)
+kobe2.days_off = kobe2.days_off.astype('timedelta64[D]')
+kobe2.days_off = kobe2.days_off.apply(lambda x: x-1)
+kobe2.days_off = kobe2.days_off.apply(lambda x: x==10 if x>=11 else x)
+kobe2.days_off.max()
+
 
 print kobe2.columns
 kobe2.combined_shot_type.value_counts()
@@ -122,7 +177,7 @@ feature_cols = ['loc_x', 'loc_y', 'seconds_remaining', 'shot_distance', '1996-97
                 '1997-98', '1998-99', '1999-00', '2000-01', '2001-02', '2002-03',
                 '2003-04', '2004-05', '2005-06', '2006-07', '2007-08', '2008-09',
                 '2009-10', '2010-11', '2011-12', '2012-13', '2013-14', '2014-15',
-                '2015-16', 'playoffs', 'game_event_id']
+                '2015-16', 'playoffs', 'game_event_id', '']
 
 # Let the modelling begin
 X = kobe2[feature_cols]
@@ -135,21 +190,32 @@ from sklearn import metrics
 metrics.accuracy_score(y_test, y_class_pred)
 lr.score(X_test, y_test)
 
-# After looking for some more useful variables. I think it might be useful to create dummies
-# for the 'combined shot type' and see if that helps.
 
-shot_dummies = pd.get_dummies(kobe2.combined_shot_type)
-
-kobe2 = kobe2.join(shot_dummies)
 
 kobe2.columns
+kobe2.action_type.value_counts()
 
-feature_cols = ['loc_x', 'loc_y', 'seconds_remaining', 'shot_distance', '1996-97',
-                '1997-98', '1998-99', '1999-00', '2000-01', '2001-02', '2002-03',
-                '2003-04', '2004-05', '2005-06', '2006-07', '2007-08', '2008-09',
-                '2009-10', '2010-11', '2011-12', '2012-13', '2013-14', '2014-15',
-                '2015-16', 'playoffs', 'Bank Shot', 'Dunk', 'Hook Shot',
-                'Jump Shot', 'Layup', 'Tip Shot', 'minutes_remaining']
+
+kobe2.shape
+period_dummies = pd.get_dummies(kobe2.period)
+
+kobe2 =kobe2.join(period_dummies)
+
+# Calculate how much time has elapsed in the game
+action_cols
+feature_cols = ['time_left','lat', 'lon','Alley Oop Dunk Shot', 'Alley Oop Layup shot',
+                'Cutting Layup Shot', 'Driving Bank shot', 'Driving Dunk Shot', 'Driving Finger Roll Layup Shot',
+                'Driving Finger Roll Shot', 'Driving Floating Bank Jump Shot', 'Driving Floating Jump Shot',
+                'Driving Hook Shot', 'Driving Jump shot', 'Driving Layup Shot', 'Driving Reverse Layup Shot',
+                'Driving Slam Dunk Shot', 'Dunk Shot', 'Fadeaway Bank shot', 'Fadeaway Jump Shot', 'Finger Roll Layup Shot',
+                'Finger Roll Shot', 'Floating Jump shot', 'Follow Up Dunk Shot', 'Hook Bank Shot',
+                'Hook Shot', 'Jump Bank Shot', 'Jump Hook Shot', 'Jump Shot', 'Layup Shot', 'Pullup Bank shot', 'Pullup Jump shot',
+                'Putback Dunk Shot', 'Putback Layup Shot', 'Reverse Slam Dunk Shot', 'Running Bank shot', 'Running Dunk Shot',
+                'Running Finger Roll Layup Shot', 'Running Finger Roll Shot', 'Running Hook Shot', 'Running Jump Shot', 'Running Layup Shot',
+                'Running Pull-Up Jump Shot', 'Running Reverse Layup Shot', 'Running Slam Dunk Shot', 'Running Tip Shot',
+                'Slam Dunk Shot', 'Step Back Jump shot', 'Tip Layup Shot', 'Tip Shot', 'Turnaround Bank shot', 'Turnaround Fadeaway shot',
+                'Turnaround Finger Roll Shot', 'Turnaround Hook Shot', 'Turnaround Jump Shot', 'home', 'playoffs',
+                'shot_distance', 'days_off']
 
 X = kobe2[feature_cols]
 y = kobe2.shot_made_flag
@@ -165,3 +231,5 @@ y_class_pred = lr.predict(X_test)
 from sklearn import metrics
 metrics.accuracy_score(y_test, y_class_pred)
 lr.score(X_test, y_test)
+
+kobe2.period.value_counts()
